85082ab1b08e328cfc2015448cf5fbd76692ae9a
Andre Flemming <DasLampe@Andres-MacBook-Pro.local>
2013-03-13 01:51:35 +0100

    Integrate Twitter bootstrap
    Remove: git clone form twitter bootstrap
    Add: download from twitter bootstrap

--- template/layout.php
+++ template/layout.php
@@ -1,8 +1,13 @@
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap -->
+	<link href="<?= LINK_MAIN; ?>template/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="<?= LINK_MAIN; ?>template/lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 </head>
 <body>
-	<h1>Test!</h1>
-	{page_content}
+	<div class="container">
+		{page_content}
+	</div>
 </body>
 </html>
