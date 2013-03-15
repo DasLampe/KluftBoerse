ec80020422905149abc3660ce723e7ce1dc2795e
DasLampe <daslampe@lano-crew.org>
2013-03-15 17:33:58 +0100

    FIXED: CSS f& JS files from view class

--- template/layout.php
+++ template/layout.php
@@ -4,6 +4,8 @@
 	<!-- Bootstrap -->
 	<link href="<?= LINK_MAIN; ?>template/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 	<link href="<?= LINK_MAIN; ?>template/lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+	{css_files}
+	{js_files}
 </head>
 <body>
 	<div class="container">
