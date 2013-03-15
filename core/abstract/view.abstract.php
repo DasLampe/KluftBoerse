864df4c4999613ecbadf146b81c5317771560ea4
DasLampe <daslampe@lano-crew.org>
2013-03-15 17:32:13 +0100

    ADD: User logger in abstract view.

--- core/abstract/view.abstract.php
+++ core/abstract/view.abstract.php
@@ -8,11 +8,15 @@ abstract class AbstractView {
 	protected $model;
 	
 	public function __construct() {
-		$name			= str_replace('View', '', get_class($this));
-		$classname		= $name."Model";
+		try {
+			$name			= str_replace('View', '', get_class($this));
+			$classname		= $name."Model";
 		
-		exception_include(PATH_APP.lcfirst($name)."/model.php");
-		$this->model	= new $classname();
+			exception_include(PATH_APP.lcfirst($name)."/model.php");
+			$this->model	= new $classname();
+		} catch(Exception $e) {
+			ramverkLog::setWarning($e->getMessage(), __FILE__);
+		}
 			
 		$this->tpl		= ramverkTemplate::getInstance();
 	}
