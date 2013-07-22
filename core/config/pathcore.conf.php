75ef15ae183811fa8937aa6b993edc4a0e37fe7c
DasLampe <daslampe@lano-crew.org>
2013-07-22 20:07:35 +0200

    ADD: Plugin structure

--- core/config/pathcore.conf.php
+++ core/config/pathcore.conf.php
@@ -17,7 +17,9 @@ if(strlen($_SERVER['DOCUMENT_ROOT']) < strlen($dir[0]))
 define("PATH_MAIN",				$dir[0]);
 define("PATH_CORE",				PATH_MAIN."core/");
 define("PATH_APP",				PATH_MAIN."app/");
+define("PATH_PLUGIN",			PATH_MAIN."lib/plugin/");
 
 define("LINK_MAIN",				$protocol."://".$_SERVER['HTTP_HOST'].$subdir);
+define("DOMAIN",				$_SERVER['HTTP_HOST']);
 
 define("CURRENT_PAGE",			LINK_MAIN.substr($_SERVER['QUERY_STRING'], 6));
